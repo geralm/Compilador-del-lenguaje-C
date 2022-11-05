57c7840a29738983cc636164f7a44c3a42cb851a
Esteban <estebanlm@estudiantec.cr>
2022-11-04 22:38:53 -0600

    Update AppTest.java

--- CompiladorC/src/test/java/CompiladorC/AppTest.java
+++ CompiladorC/src/test/java/CompiladorC/AppTest.java
@@ -4,19 +4,25 @@ package CompiladorC;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.StringReader;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+public class AppTest
 {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
+    public void parserAnaylysis()throws Exception
     {
-        assertTrue( true );
+        String expresion = "int i = 5;";
+        LexerCupAnalyzer lexer = new LexerCupAnalyzer(new StringReader(expresion));
+        Parser p = new Parser(lexer);
+        Integer resultado = (Integer) p.parse().value;
+        System.out.println(resultado);
     }
 }
