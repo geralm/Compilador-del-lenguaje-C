0f4966e6882b1ae08a184d9ada603de21fb9669b
Daniel Araya Sambucci <danielarayasambucci@estudiantec.cr>
2022-11-07 16:22:28 -0600

    Gramatic finished. Waiting to fix if else error

--- CompiladorC/src/test/java/CompiladorC/AppTest.java
+++ CompiladorC/src/test/java/CompiladorC/AppTest.java
@@ -20,7 +20,8 @@ public class AppTest
     public void parserAnaylysis()throws Exception
     {
         //String expresion = "int prueba(){if (a == b){int hola = 0; for(i = 0; i < 5; i++){}}else{} return 0;}";
-        String expresion = "static int prueba(){if (a == b){int hola = 0; while(i < 5){}}else{} return 0;}";
+        //String expresion = "static int prueba(int i, long prueba[]){if (a == b){int hola = 0; while(i < 5){}}else{} return 0;}";
+        String expresion = "int prueba(){switch(num){case 7: {printf(); break; default: {printf(\"hola\");break;}}}}";
         LexerCupAnalyzer lexer = new LexerCupAnalyzer(new StringReader(expresion));
         Parser p = new Parser(lexer);
         //Integer resultado = (Integer) p.parse().value;
